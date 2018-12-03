166b512d99a270992727f174d65a92a8c9b0022e
Trevor Nagy <tnagy@nccinc.ca>
2018-12-02 22:26:13 -0500

    Added files for the videos project.

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+import config from "../config";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: config.youtubeApi
+  }
+});
